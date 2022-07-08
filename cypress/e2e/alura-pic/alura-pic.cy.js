e4cd0be9e0e89b717934c762a5fed4f5182ae587
MarianaMSA <maiasalvador@gmail.com>
2022-07-08 13:20:08 -0300

    teste com dashborard

--- cypress/e2e/alura-pic/alura-pic.cy.js
+++ cypress/e2e/alura-pic/alura-pic.cy.js
@@ -11,26 +11,26 @@ describe("Login e registro de usuário alura pic", () => {
     cy.contains("ap-vmessage", "User name is required!").should("be.visible");
     cy.contains("ap-vmessage", "Password is required!").should("be.visible");
   });
-  it("verificação mensagem de e-mail inválido", () => {
+  it("verifica mensagem de e-mail inválido", () => {
     cy.contains("a", "Register now").click();
     cy.get('input[formcontrolname="email"]').type("mariana");
     cy.contains("button", "Register").click();
     cy.contains("ap-vmessage", "Invalid e-mail").should("be.visible");
   });
-  it("verificação mensagem de senha inválida", () => {
+  it("verifica mensagem de senha inválida", () => {
     cy.contains("a", "Register now").click();
     cy.get('input[formcontrolname="password"]').type("mariana");
     cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
+    cy.contains("ap-vmessage", "Mininum length is").should("be.visible");
   });
-  it("verificação mensagem de senha com menos de 8 caracteres", () => {
+  it("verifica mensagem de senha com menos de 8 caracteres", () => {
     cy.contains("a", "Register now").click();
     cy.get('input[formcontrolname="password"]').type("mariana");
     cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
+    cy.contains("ap-vmessage", "Mininum length is ").should("be.visible");
   });
 
-  it("verificação mensagem de letra maiuscula no user name", () => {
+  it("verifica mensagem de letra maiuscula no user name", () => {
     cy.contains("a", "Register now").click();
     cy.get('input[formcontrolname="userName"]').type("MARIANA");
     cy.contains("button", "Register").click();
